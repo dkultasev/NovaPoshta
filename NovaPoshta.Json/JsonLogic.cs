9e8af4cfafe34b8d7b91d4b98fadb7adb6b9f30d
Dima <dkultasev@mail.ru>
2017-04-10 10:08:54 +0300

    small refactor

--- NovaPoshta.Json/JsonLogic.cs
+++ NovaPoshta.Json/JsonLogic.cs
@@ -19,13 +19,29 @@ namespace NovaPoshta.Json
 
         public IEnumerable<T> GetJsonData<T>(string modelName, string calledMethod, dynamic methodProperties)
         {
-            var result = GetObjectByRequest<T>(modelName, calledMethod, methodProperties);
+            var jqr = new JsonRequestRoot()
+            {
+                apiKey = _config.ApiKey,
+                modelName = modelName,
+                calledMethod = calledMethod,
+                methodProperties = methodProperties
+            };
+
+            dynamic result = GetObjectByRequest<T>(jqr);
             return result[0].data;
         }
 
         public T GetJsonRootData<T>(string modelName, string calledMethod, dynamic methodProperties)
         {
-            var result = GetObjectRootByRequest<T>(modelName, calledMethod, methodProperties);
+            var jqr = new JsonRequestRoot()
+            {
+                apiKey = _config.ApiKey,
+                modelName = modelName,
+                calledMethod = calledMethod,
+                methodProperties = methodProperties
+
+            };
+            RootObject<T> result = GetObjectRootByRequest<T>(jqr);
             if (result.errors?.Count > 0)
             {
                 throw new ArgumentException(string.Join("\n", result.errors));
@@ -35,43 +51,37 @@ namespace NovaPoshta.Json
         }
 
 
-        private IEnumerable<RootObject<T>> GetObjectByRequest<T>(string modelName, string calledMethod, dynamic methodProperties)
+        private IEnumerable<RootObject<T>> GetObjectByRequest<T>(JsonRequestRoot jqr)
         {
-            _client.BaseUrl = _config.ApiUrl; 
+            _client.BaseUrl = _config.ApiUrl;
 
-            var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
-            var jqr = new JsonRequestRoot()
+            var request = new RestRequest(Method.POST)
             {
-                apiKey = _config.ApiKey,
-                modelName = modelName,
-                calledMethod = calledMethod,
-                methodProperties = methodProperties
+                RequestFormat = DataFormat.Json,
+                JsonSerializer = new RestSharpJsonNetSerializer("yyyy-MM-dd HH:mm:ss")
             };
-            request.JsonSerializer = new RestSharpJsonNetSerializer("yyyy-MM-dd HH:mm:ss");
+
             request.AddBody(jqr);
             request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
             var result = _client.Execute<List<RootObject<T>>>(request);
             return result.Data;
         }
 
-        private RootObject<T> GetObjectRootByRequest<T>(string modelName, string calledMethod, dynamic methodProperties)
+        private RootObject<T> GetObjectRootByRequest<T>(JsonRequestRoot jqr)
+        {
+            var request = PrepareRequest(jqr);
+            var result = _client.Execute<RootObject<T>>(request).Data;
+            return result;
+        }
+
+        private IRestRequest PrepareRequest(JsonRequestRoot jqr)
         {
             _client.BaseUrl = _config.ApiUrl;
-
-            var request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
-            var jqr = new JsonRequestRoot()
-            {
-                apiKey = _config.ApiKey,
-                modelName = modelName,
-                calledMethod = calledMethod,
-                methodProperties = methodProperties
-
-            };
+            IRestRequest request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
             request.JsonSerializer = new RestSharpJsonNetSerializer("yyyy-MM-dd HH:mm:ss");
             request.AddBody(jqr);
             request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
-            var result = _client.Execute<RootObject<T>>(request).Data;
-            return result;
+            return request;
         }
 
     }
---
+++ NovaPoshta.Tests/RestClientTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Net;
+using Moq;
+using NovaPoshta.Core;
+using NovaPoshta.Entity;
+using NovaPoshta.Json;
+using NUnit.Framework;
+using RestSharp;
+
+namespace NovaPoshta.Tests
+{
+    public class RestClientTests
+    {
+        [Test]
+        public void When_SimpleRequest_Then_StatusOK()
+        {
+            Mock<IRestClient> mockClient = new Mock<IRestClient>();
+
+            mockClient.Setup(x => x.Execute<Document>(It.IsAny<IRestRequest>())).Returns(new RestResponse<Document>()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = null
+            });
+
+            mockClient.Object.ExecuteAsync(new RestRequest(), (response, handle) =>
+            {
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            });
+        }
+    }
+}
