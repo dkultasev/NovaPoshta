dc52247730869c0b64c7f1b07aa80d9ba58cedd5
Dima <dkultasev@mail.ru>
2017-04-11 09:55:10 +0300

    jsonlogic refactor

--- NovaPoshta.Json/JsonLogic.cs
+++ NovaPoshta.Json/JsonLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using RestSharp;
 
 namespace NovaPoshta.Json
@@ -19,29 +20,14 @@ namespace NovaPoshta.Json
 
         public IEnumerable<T> GetJsonData<T>(string modelName, string calledMethod, dynamic methodProperties)
         {
-            var jqr = new JsonRequestRoot()
-            {
-                apiKey = _config.ApiKey,
-                modelName = modelName,
-                calledMethod = calledMethod,
-                methodProperties = methodProperties
-            };
-
-            dynamic result = GetObjectByRequest<T>(jqr);
-            return result[0].data;
+            IRestResponse<List<RootObject<T>>> result = _client.Execute<List<RootObject<T>>>(PrepareRequest(modelName, calledMethod, methodProperties));
+            return result.Data[0].data;
         }
 
         public T GetJsonRootData<T>(string modelName, string calledMethod, dynamic methodProperties)
         {
-            var jqr = new JsonRequestRoot()
-            {
-                apiKey = _config.ApiKey,
-                modelName = modelName,
-                calledMethod = calledMethod,
-                methodProperties = methodProperties
 
-            };
-            RootObject<T> result = GetObjectRootByRequest<T>(jqr);
+            RootObject<T> result = _client.Execute<RootObject<T>>(PrepareRequest(modelName, calledMethod, methodProperties)).Data;
             if (result.errors?.Count > 0)
             {
                 throw new ArgumentException(string.Join("\n", result.errors));
@@ -50,32 +36,17 @@ namespace NovaPoshta.Json
             return result.data[0];
         }
 
-
-        private IEnumerable<RootObject<T>> GetObjectByRequest<T>(JsonRequestRoot jqr)
+        private IRestRequest PrepareRequest(string modelName, string calledMethod, dynamic methodProperties)
         {
-            _client.BaseUrl = _config.ApiUrl;
-
-            var request = new RestRequest(Method.POST)
+            var jqr = new JsonRequestRoot()
             {
-                RequestFormat = DataFormat.Json,
-                JsonSerializer = new RestSharpJsonNetSerializer("yyyy-MM-dd HH:mm:ss")
+                apiKey = _config.ApiKey,
+                modelName = modelName,
+                calledMethod = calledMethod,
+                methodProperties = methodProperties
+
             };
 
-            request.AddBody(jqr);
-            request.OnBeforeDeserialization = resp => { resp.ContentType = "application/json"; };
-            var result = _client.Execute<List<RootObject<T>>>(request);
-            return result.Data;
-        }
-
-        private RootObject<T> GetObjectRootByRequest<T>(JsonRequestRoot jqr)
-        {
-            var request = PrepareRequest(jqr);
-            var result = _client.Execute<RootObject<T>>(request).Data;
-            return result;
-        }
-
-        private IRestRequest PrepareRequest(JsonRequestRoot jqr)
-        {
             _client.BaseUrl = _config.ApiUrl;
             IRestRequest request = new RestRequest(Method.POST) { RequestFormat = DataFormat.Json };
             request.JsonSerializer = new RestSharpJsonNetSerializer("yyyy-MM-dd HH:mm:ss");
