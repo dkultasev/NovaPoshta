9b7664b1808791ae88cca03e69316d472356a841
Dima <dkultasev@mail.ru>
2017-05-19 14:58:33 +0300

    Add unit tests for restclient responses errors

--- NovaPoshta.UnitTests/JsonLogicTests.cs
+++ NovaPoshta.UnitTests/JsonLogicTests.cs
@@ -11,11 +11,46 @@ namespace NovaPoshta.UnitTests
     public class JsonLogicTests
     {
         private Mock<IRestClient> _clientMock;
+        private NovaPoshtaConfig _config;
 
         [SetUp]
         public void Init()
         {
             _clientMock = new Mock<IRestClient>();
+            _config = new InitialSetup().Config;
+        }
+
+        [Test]
+        public void Test_When_There_Are_Errors_Returned_Then_GetListOfObjects_Throw_Exception()
+        {
+            _clientMock.Setup(cl => cl.Execute<List<RootObject<object>>>(It.IsAny<IRestRequest>()))
+                .Returns(new RestResponse<List<RootObject<object>>>()
+                {
+                   Data = new List<RootObject<object>>()
+                   {
+                       new RootObject<object>()
+                       {
+                           errors =  new List<object> { "1", "2" }
+                       }
+                   }
+                });
+
+            ((Action)(() => new JsonLogic(_clientMock.Object, _config).GetListOfObjects<object>("","",""))).ShouldThrowExactly<ResponseException>();
+        }
+
+        [Test]
+        public void Test_When_There_Are_Errors_Returned_Then_ModifyObject_Throw_Exception()
+        {
+            _clientMock.Setup(cl => cl.Execute<RootObject<object>>(It.IsAny<IRestRequest>()))
+                .Returns(new RestResponse<RootObject<object>>()
+                {
+                   Data = new RootObject<object>()
+                   {
+                       errors = new List<object> { "1", "2" }
+                   }
+                });
+
+            ((Action)(() => new JsonLogic(_clientMock.Object, _config).ModifyObject<object>("","",""))).ShouldThrowExactly<ResponseException>();
         }
 
         [Test]
